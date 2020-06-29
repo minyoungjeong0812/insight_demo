f3e63dddbc021ee5d7f89abecf325ea321d9b8f9
Minyoung Jeong <jeongmin0812@gmail.com>
2020-06-28 19:24:56 -0700

    post cleanup

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+#loading neccessary packages in
 import pandas as pd
 import os
 
@@ -10,6 +11,8 @@ from operator import itemgetter
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as bs
 
+import plotly.express as px
+from plotly.subplots import make_subplots
 
 import preprocess
 import detect
@@ -32,12 +35,8 @@ import dash_html_components as html
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
+#flask server initiation
 server = Flask(__name__)
-#DESKTOP
-
-##server._static_folder = "/home/minyoung/Documents/insight_week2/static"
-
-#LAPTOP
 
 # Configure session to use filesystem
 server.config["SESSION_PERMANENT"] = False
@@ -49,13 +48,15 @@ engine = create_engine(
     "postgres://ksfkoirmqqkghq:8843ee1b00e9df0948a10b662918ce11131f75859826aca4fecae08f088fdc8e@ec2-52-207-25-133.compute-1.amazonaws.com:5432/d6k5uvvu11hsqa")
 db = scoped_session(sessionmaker(bind=engine))
 
-
+#indicator list to create dropdown on dash web app page
 available_indicators = ['kill','headshot','kill_per_min','headshot_per_kill','damage']
-######################
+
+#dash web app creation
 dash_app1 = dash.Dash(__name__, server=server, url_base_pathname='/dashboard1/', external_stylesheets=external_stylesheets)
 
-# dash_app1 : my own again
+
 dash_app1.layout = html.Div([
+
     # represents the URL bar, doesn't render anything
     dcc.Location(id='url', refresh=False),
 
@@ -86,34 +87,40 @@ dash_app1.layout = html.Div([
               [dash.dependencies.Input('url', 'href'),Input('yaxis-column', 'value')])
 
 def display_page(pathname,parameter):
+    #importing url address to parse out the user id only
     import urllib.parse
     parsed_url = urllib.parse.urlparse(pathname)
     parsed_query = urllib.parse.parse_qs(parsed_url.query)
     name = parsed_query['username'][0]
+
+    #loading in the data from heroku sql database to pandas dataframe
     df=pd.read_sql_query('select playdate, playtime, title, gametype,kill,headshot,damage from "all_users_raw"',con=engine).drop_duplicates(subset=['playdate', 'gametype', 'headshot', 'playtime'])
 
+    #excluding "event" game type : event game type will result in unusual 30-40 kills in 10 mins even for general users
     df = df[df['gametype'] != 'Event']
+
+    #getting kill_per_min feature (kills per minute)
     df['kill_per_min'] = 60*df['kill']/df['playtime']
+
+    #getting headshot_per_min feature (headshot per minute)
     df['headshot_per_kill'] = df['headshot']/df['kill']
+
+    #converting user id (title) to lower case for easier finding
     df['title2']=df['title'].str.lower()
 
+    #resetting and setting dataframe index to playdate for easier plotting
     df.reset_index(inplace=True)
     df.set_index("playdate", inplace=True)
 
+    #finding the rows in the dataframe that belongs to interested (serached) user and saving it as df2
     df2 = df[df['title2'] == name.lower()]
 
-    dfg=df.groupby('gametype')
-    print(dfg.ngroups)
-
-    import plotly.express as px
-
-    ##Fig: only suspicious
+    ##Figure creation (fig : seperate user data histogram, fig 3: separate time series user data )
     fig = px.histogram(df2, x=parameter, nbins=10, color="gametype", marginal="rug", hover_data=df2.columns)
 
     fig3 = go.Figure(data=go.Scatter(x=df2.index,y=df2[parameter],mode='markers'))
 
-    ##Fig2: all users
-    from plotly.subplots import make_subplots
+    #Figure creation (fig2: overlaid general user + selected user histogram data for different game type)
     fig2 = make_subplots(
     rows=4, cols=2,shared_xaxes=False, vertical_spacing=0.1,
     subplot_titles=("Squad", "Duo", "Solo", "Ranked FPP","Solo FPP","Duo FPP","Squad FPP"))
@@ -139,7 +146,7 @@ def display_page(pathname,parameter):
     fig2.append_trace(go.Histogram(x=df[df['gametype']=='Squad FPP'][parameter], histnorm='probability',bingroup=6,marker_color='#707070',opacity=0.9,nbinsx=10, name="General user - Squad FPP"),row=4,col=1)
     fig2.add_trace(go.Histogram(x=df2[df2['gametype']=='Squad FPP'][parameter], histnorm='probability',marker_color='#f00800',opacity=0.9,bingroup=6, nbinsx=10, name=df2['title'].iloc[0] + " - Squad FPP"),row=4,col=1)
 
-# Update xaxis properties
+    # Update xaxis properties
     fig2.update_xaxes(title_text=parameter.upper(), row=1, col=1)
     fig2.update_xaxes(title_text=parameter.upper(), row=1, col=2)
     fig2.update_xaxes(title_text=parameter.upper(), row=2, col=1)
@@ -162,7 +169,7 @@ def display_page(pathname,parameter):
     return html.Div([html.H3('Player : {}'.format(df2['title'].iloc[0]))]), dcc.Graph(id='example-graph',figure=fig,style={'width': '100%', 'display':'inline-block'}),\
            dcc.Graph(id='example-graph',figure=fig2), dcc.Graph(id='example-graph',figure=fig3,style={'width': '100%', 'display':'inline-block'})
 
-####FLASK####
+####FLASK pages####
 @server.route('/')
 def home():
     return render_template("home.html", message="WORKING")
@@ -174,26 +181,19 @@ def check():
 
         check=request.form.get('check')
 
-        ####Adding data analysis here #####
-        print("getting data")
-        #df=pd.read_sql_query('select * from "all_users_raw"',con=engine).drop_duplicates(subset=['playdate', 'gametype', 'headshot', 'playtime'])
+        #same as above, getting the data from heroku sql database and saving it as df. Then filtering out the event game type
         df=pd.read_sql_query('select kill,headshot,damage,playtime,title,playdate,gametype from "all_users_raw"',con=engine).drop_duplicates(subset=['playdate', 'gametype', 'headshot', 'playtime'])
-
-        print("read data")
         df = df[df['gametype'] != 'Event']
-        ##Need to create df_bin##
+
+        #creating new features that can be meaningful
+        df['kill_per_min'] = 60*df['kill']/df['playtime']
+        df['head_per_kill']=df['headshot']/df['kill']
+        df['head_per_kill']=df['head_per_kill'].fillna(0)
+
+        #creating bins for user parameters
         bins = [-np.inf, 1, 3, 6, 8, 10, np.inf]
         labels = [0,1,2,3,4,5]
         df['kill_bin'] = pd.cut(df['kill'], bins=bins, labels=labels)
-
-        df['kill_per_min'] = 60*df['kill']/df['playtime']
-
-        df['head_per_kill']=df['headshot']/df['kill']
-
-        #df1=df
-        #df1=df.drop(['dbno','assists','heal','boost','revive'],axis=1)
-
-        df['head_per_kill']=df['head_per_kill'].fillna(0)
 
         bins = [-np.inf, 1, 2, 3, 4, 5, np.inf]
         labels = [0,1,2,3,4,5]
@@ -207,7 +207,6 @@ def check():
         labels = [0,1,2,3,4,5]
         df['hpk_bin'] = pd.cut(df['head_per_kill'], bins=bins, labels=labels)
 
-
         bins = [-np.inf, 0.1, 0.2, 0.3, 0.4 , 0.5, np.inf]
         labels = [0,1,2,3,4,5]
         df['kpm_bin'] = pd.cut(df['kill_per_min'], bins=bins, labels=labels)
@@ -215,34 +214,49 @@ def check():
 
         cols=['gametype','head_bin','title','kill_bin','damage_bin','kpm_bin','hpk_bin','time']
 
+        #filtering out for defined columns
         df_bin=df[cols]
 
+        #user parameter list to loop over
         parameter_list = ['kill_bin','head_bin','damage_bin','kpm_bin','hpk_bin']
-        target= int(0.0005*len(df_bin))
-        #print(len(df_bin))
-        #print(target)
-        #print(parameter)
 
+        #selecting top 1% deviators
+        target = int(0.01*df_bin['title'].nunique())
+
+        #creating an empty dataframe to store information later
         df_bad_total = pd.DataFrame(columns=['username','parameter'])
+
+        #looping over definied parameters
         for parameter in parameter_list:
+            #ratings : parameter bin, usermap : to store userid information
+            #separate preprocess file processes the data to get arrays in a needed shape
             ratings, usermap = preprocess.load_data(df_bin,parameter)
 
+            #arbitrary name
             name = 'insight_analysis'
+
+            #if use_products is 0, we get dicitonary in a format where key is a user title and values are user parameters
+            #if use_times is 0, we are not collecting temporal user behaviors. We are not interested in this.
             USE_PRODUCTS=0
             USE_TIMES=0
 
             (rating_arr, ids) = preprocess.process_data(ratings,name,USE_PRODUCTS)
             (rating_arr) = np.array(rating_arr)
 
+            #feeding in rating_arr to 'detect' function saved in a separate file
             suspn = detect.detect(rating_arr, USE_TIMES, 1)
+
+            #sorting the suspicion parameter
             susp_sorted = np.array([(x[0]) for x in sorted(enumerate(suspn), key=itemgetter(1), reverse=True)])
 
-            #print(susp_sorted)
+            #preventing out of range problem
             if target >= len(susp_sorted):
                 target=len(susp_sorted)
 
+            #list that contains bad user indices
             bad = susp_sorted[range(target)]
 
+            #empty dicitionary to contain bad user data
             dict5 = {}
 
             for i in bad:
@@ -250,18 +264,21 @@ def check():
                 value=ratings[key]
                 dict5[key]=value
 
-            #print("my dict : ", dict5)
             bad_user_id = list(dict5.keys())
+
+            #inserting obtained bad user data back to empty bad user dataframe (df_bad_total) defined above
             bad_df = pd.DataFrame(bad_user_id)
             bad_df = bad_df.rename(columns={0: 'username'})
             bad_df['parameter'] = parameter
             df_bad_total=df_bad_total.append(bad_df)
 
+        #selecting the SQL records that correspond to the serached user title
         check_res2 = db.execute("SELECT * from all_users_raw where lower(title) = :title", {"title": str(check).lower()})
 
+        #check the resultant bad user dataframe's length
         df_len_check=len(df_bad_total[df_bad_total['username'].str.lower() == check.lower()])
-        print(df_len_check)
 
+        #if statements to create different pages depending on conditions
         if df_len_check == 0 and check_res2.rowcount ==0:
             return render_template("data_load.html",check=check)
 
@@ -269,8 +286,8 @@ def check():
             return render_template("check_res2.html",check=check)
 
         else:
+            #codes to create a page if user data was in the original SQL database and bad user dataframe to show what parameters the user is suspicious for
             check_res=df_bad_total[df_bad_total['username'].str.lower() == check.lower()].to_dict()
-            #check_res=check_res['parameter']
             user=check_res['username']
             check_res=check_res['parameter']
 
@@ -286,7 +303,6 @@ def check():
                     mydict2.update({"parameter4": "Too much kills per minute"})
                 if x == "hpk_bin":
                     mydict2.update({"parameter5": "Too much headshots per kill"})
-            #check_res_list=[x for x in check_res.values()]
 
             mydict4={}
             if user:
@@ -298,11 +314,13 @@ def check():
 
     return render_template("check.html")
 
-
+#route for loading data section which will be executed when the user data does not exisit in the original user database
 @server.route("/data_load",methods=["POST"])
 def data_load():
     name= request.form.get("custId")
     print("given name: ", name)
+
+    #getting new user data from pubg.op.gg website to load in to the heroku sql database using beatuifulsoup
     url = 'https://pubg.op.gg/user/'+ name
     req = Request(url)
     webpage = urlopen(req).read()
@@ -328,7 +346,6 @@ def data_load():
           new_div_item = new_div.text.strip()
           gametype_list.append(new_div_item)
        else:
-          # print(div)
           new_div = div.find("span")
           new_div_item = new_div.text.strip()
           gametype_list.append(new_div_item)
@@ -415,13 +432,11 @@ def data_load():
     df = pd.DataFrame(dict1)
     df['playdate']=pd.to_datetime(df['playdate'], infer_datetime_format=True)
     df['damage']=df['damage'].replace({',':''},regex=True).apply(pd.to_numeric,1)
-    #print(df)
 
     message="DONE"
     df.to_sql('all_users_raw', con=engine, index=False, if_exists='append')
 
     return render_template("index.html",message=message,unique=len(df))
-
 
 
 @server.route("/analysis/<username>", methods=["GET", "POST"])
